488f84d7b8e60d0a92d3303bcd4a45f2bd0f3111
Renato Carvallhan <rcarvalhan@gmail.com>
2017-10-03 16:43:47 -0300

    starting devicesviewcontroller

--- Example/CarenetSDK/ViewController.swift
+++ Example/CarenetSDK/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CarenetSDK
 
 class ViewController: UIViewController {
 
@@ -15,9 +16,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        CarentSDK.shared.startSDKWithClientId()
     }
 
 }
